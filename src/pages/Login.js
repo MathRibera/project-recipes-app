9af5a5c6c79090cb0fd902504bbd31d1b2b031aa
Thayane <thayanekcr@gmail.com>
2023-03-02 17:58:25 -0300

    Requisito 1
    feat: requisitos 2 e 3 passando

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const authPass = 6;
+  const regexEmail = /^([a-z0-9_.-]+)@([\da-z.-]+)\.([a-z.]{2,6})$/;
+
   return (
-    <div>Login</div>
+    <div>
+      <label htmlFor="input-email">
+        <input
+          value={ email }
+          onChange={ ({ target }) => setEmail(target.value) }
+          id="input-email"
+          type="email"
+          data-testid="email-input"
+        />
+      </label>
+      <label htmlFor="input-password">
+        <input
+          value={ password }
+          onChange={ ({ target }) => setPassword(target.value) }
+          type="password"
+          data-testid="password-input"
+        />
+      </label>
+      <button
+        disabled={ password.length < authPass || !regexEmail.test(email) }
+        data-testid="login-submit-btn"
+        type="button"
+        onClick={
+          localStorage.setItem('user', JSON.stringify({ email: `${email}` }))
+        }
+      >
+        Enter
+      </button>
+    </div>
   );
 }
 
