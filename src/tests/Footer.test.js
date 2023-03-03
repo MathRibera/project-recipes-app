67fde8518485521463560f611bd705bc0e011d3e
Matheus Jorge <bs.fenixfl@gmail.com>
2023-03-03 12:52:50 -0300

    Fix: testes
    removido waitfor nao utilizado

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -1,6 +1,6 @@
 import userEvent from '@testing-library/user-event';
 import React from 'react';
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 
