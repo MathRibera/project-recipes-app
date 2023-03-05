1d8c6ac974f49b4ddbfe0e46014f3208fc9476bb
Matheus Jorge <bs.fenixfl@gmail.com>
2023-03-05 14:35:56 -0300

    fix: Fixado erros do tests

--- src/tests/Searchbar.test.js
+++ src/tests/Searchbar.test.js
@@ -6,21 +6,26 @@ import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 
 describe('end to end basicao', () => {
+  const firstLetter = 'first-letter-search-radio';
+  const imageCard = '0-card-img';
+  const emailInput = 'email-input';
+  const password = 'password-input';
+  const searchInput = 'search-input';
   it('end to end search bar', async () => {
     const { history } = renderWithRouter(<App />);
     const loginInput = 'login-submit-btn';
-    expect(screen.getByTestId('email-input'));
-    expect(screen.getByTestId('password-input'));
+    expect(screen.getByTestId(emailInput));
+    expect(screen.getByTestId(password));
     expect(screen.getByTestId(loginInput)).toBeDisabled();
 
-    userEvent.type(screen.getByTestId('email-input'), 'test@test.com');
+    userEvent.type(screen.getByTestId(emailInput), 'test@test.com');
     expect(screen.getByTestId(loginInput)).toBeDisabled();
-    userEvent.type(screen.getByTestId('password-input'), '1234567');
+    userEvent.type(screen.getByTestId(password), '1234567');
     expect(screen.getByTestId(loginInput)).toBeEnabled();
     userEvent.click(screen.getByTestId(loginInput));
 
     userEvent.click(screen.getByTestId('button-search'));
-    const inputText = screen.getByTestId('search-input');
+    const inputText = screen.getByTestId(searchInput);
     const buttonSearch = screen.getByTestId('exec-search-btn');
 
     userEvent.type(inputText, 'potato');
@@ -31,39 +36,38 @@ describe('end to end basicao', () => {
     userEvent.type(inputText, 'potato');
     userEvent.click(screen.getByTestId('name-search-radio'));
     userEvent.click(buttonSearch);
-    await waitFor(() => expect(screen.findByTestId('0-card-img')), { interval: 3000 });
-    userEvent.click(screen.getByTestId('first-letter-search-radio'));
+    await waitFor(() => expect(screen.findByTestId(imageCard)), { interval: 3000 });
+    userEvent.click(screen.getByTestId(firstLetter));
     userEvent.type(inputText, 'a');
     userEvent.click(buttonSearch);
-    await waitFor(() => expect(screen.findByTestId('0-card-img')), { interval: 3000 });
+    await waitFor(() => expect(screen.findByTestId(imageCard)), { interval: 3000 });
   });
   it('testa drink end to end', async () => {
-    const { history } = renderWithRouter(<App />);
+    renderWithRouter(<App />);
     const loginInput = 'login-submit-btn';
-    expect(screen.getByTestId('email-input'));
-    expect(screen.getByTestId('password-input'));
+    expect(screen.getByTestId(emailInput));
+    expect(screen.getByTestId(password));
     expect(screen.getByTestId(loginInput)).toBeDisabled();
 
-    userEvent.type(screen.getByTestId('email-input'), 'test@test.com');
+    userEvent.type(screen.getByTestId(emailInput), 'test@test.com');
     expect(screen.getByTestId(loginInput)).toBeDisabled();
-    userEvent.type(screen.getByTestId('password-input'), '1234567');
+    userEvent.type(screen.getByTestId(password), '1234567');
     expect(screen.getByTestId(loginInput)).toBeEnabled();
     userEvent.click(screen.getByTestId(loginInput));
-
 
     userEvent.click(screen.getByTestId('drinkButton'));
     userEvent.click(screen.getByTestId('button-search'));
     const buttonSearch = screen.getByTestId('exec-search-btn');
-    userEvent.type(screen.getByTestId('search-input'), 'lime');
+    userEvent.type(screen.getByTestId(searchInput), 'lime');
     userEvent.click(buttonSearch);
-    await waitFor(() => expect(screen.findByTestId('0-card-img')), { interval: 3000 });
-    userEvent.click(screen.getByTestId('first-letter-search-radio'));
+    await waitFor(() => expect(screen.findByTestId(imageCard)), { interval: 3000 });
+    userEvent.click(screen.getByTestId(firstLetter));
     userEvent.click(screen.getByTestId('ingredient-search-radio'));
-    userEvent.click(screen.getByTestId('first-letter-search-radio'));
-    userEvent.type(screen.getByTestId('search-input'), 'a');
+    userEvent.click(screen.getByTestId(firstLetter));
+    userEvent.type(screen.getByTestId(searchInput), 'a');
     userEvent.click(buttonSearch);
-    await waitFor(() => expect(screen.findByTestId('0-card-img')), { interval: 3000 });
-    userEvent.click(screen.getByTestId('first-letter-search-radio'));
-    userEvent.type(screen.getByTestId('search-input'), 'aa');
+    await waitFor(() => expect(screen.findByTestId(imageCard)), { interval: 3000 });
+    userEvent.click(screen.getByTestId(firstLetter));
+    userEvent.type(screen.getByTestId(searchInput), 'aa');
   });
 });
