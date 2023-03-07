4478a2bddd9bd7b93d42ab87afb8a75826f07620
Matheus Jorge <bs.fenixfl@gmail.com>
2023-03-07 18:29:20 -0300

    alteraçoes para realizar o merge

--- src/pages/RecipeInProgressMeal.js
+++ src/pages/RecipeInProgressMeal.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
 
 function RecipeInProgressMeal() {
-  const { id } = useParams();
+  // const { id } = useParams();
   useEffect(() => {
 
   }, []);
