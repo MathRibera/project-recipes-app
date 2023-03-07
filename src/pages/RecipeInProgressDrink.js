69f953a654ec06f2996af20e09c2882cb2d52466
Matheus Jorge <bs.fenixfl@gmail.com>
2023-03-07 18:15:10 -0300

    alteraçoes para realizar o merge

--- src/pages/RecipeInProgressDrink.js
+++ src/pages/RecipeInProgressDrink.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 function RecipeInProgressDrink() {
-  const [drink, setDrink] = useState([]);
+  // const [drink, setDrink] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
@@ -15,42 +15,42 @@ function RecipeInProgressDrink() {
     };
     fetchApi();
   }, []);
-  const recipies = [
-    drink.strIngredient1,
-    drink.strIngredient2,
-    drink.strIngredient3,
-    drink.strIngredient4,
-    drink.strIngredient5,
-    drink.strIngredient6,
-    drink.strIngredient7,
-    drink.strIngredient8,
-    drink.strIngredient9,
-    drink.strIngredient10,
-    drink.strIngredient11,
-    drink.strIngredient12,
-    drink.strIngredient13,
-    drink.strIngredient14,
-    drink.strIngredient15,
-  ];
-  const measures = [
-    drink.strMeasure1,
-    drink.strMeasure2,
-    drink.strMeasure3,
-    drink.strMeasure4,
-    drink.strMeasure5,
-    drink.strMeasure6,
-    drink.strMeasure7,
-    drink.strMeasure8,
-    drink.strMeasure9,
-    drink.strMeasure10,
-    drink.strMeasure11,
-    drink.strMeasure12,
-    drink.strMeasure13,
-    drink.strMeasure14,
-    drink.strMeasure15,
-  ];
-  const newMeasures = measures.filter((e) => typeof e === 'string' && e.length);
-  const newRecipes = recipies.filter((e) => typeof e === 'string' && e.length);
+  // const recipies = [
+  //   drink.strIngredient1,
+  //   drink.strIngredient2,
+  //   drink.strIngredient3,
+  //   drink.strIngredient4,
+  //   drink.strIngredient5,
+  //   drink.strIngredient6,
+  //   drink.strIngredient7,
+  //   drink.strIngredient8,
+  //   drink.strIngredient9,
+  //   drink.strIngredient10,
+  //   drink.strIngredient11,
+  //   drink.strIngredient12,
+  //   drink.strIngredient13,
+  //   drink.strIngredient14,
+  //   drink.strIngredient15,
+  // ];
+  // const measures = [
+  //   drink.strMeasure1,
+  //   drink.strMeasure2,
+  //   drink.strMeasure3,
+  //   drink.strMeasure4,
+  //   drink.strMeasure5,
+  //   drink.strMeasure6,
+  //   drink.strMeasure7,
+  //   drink.strMeasure8,
+  //   drink.strMeasure9,
+  //   drink.strMeasure10,
+  //   drink.strMeasure11,
+  //   drink.strMeasure12,
+  //   drink.strMeasure13,
+  //   drink.strMeasure14,
+  //   drink.strMeasure15,
+  // ];
+  // const newMeasures = measures.filter((e) => typeof e === 'string' && e.length);
+  // const newRecipes = recipies.filter((e) => typeof e === 'string' && e.length);
   return (
     <div>
       aaa
