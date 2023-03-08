2580c6230e779f2eec0524d64c19973a6d4c9f8f
Matheus Jorge <bs.fenixfl@gmail.com>
2023-03-08 17:25:42 -0300

    Testes do searchbar

--- src/tests/Searchbar.test.js
+++ src/tests/Searchbar.test.js
@@ -6,6 +6,7 @@ import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 
 describe('end to end basicao', () => {
+  global.alert = jest.fn();
   const firstLetter = 'first-letter-search-radio';
   const imageCard = '0-card-img';
   const emailInput = 'email-input';
@@ -13,8 +14,8 @@ describe('end to end basicao', () => {
   const searchInput = 'search-input';
   const searchBarBtn = 'exec-search-btn';
   it('end to end search bar', async () => {
-    const { history } = renderWithRouter(<App />);
     const loginInput = 'login-submit-btn';
+    const { history } = renderWithRouter(<App />);
     expect(screen.getByTestId(emailInput));
     expect(screen.getByTestId(password));
     expect(screen.getByTestId(loginInput)).toBeDisabled();
@@ -25,26 +26,31 @@ describe('end to end basicao', () => {
     expect(screen.getByTestId(loginInput)).toBeEnabled();
     userEvent.click(screen.getByTestId(loginInput));
 
+    await screen.findByTestId('button-search');
     userEvent.click(screen.getByTestId('button-search'));
-    const inputText = screen.getByTestId(searchInput);
+    const inputText = await screen.findByTestId(searchInput);
     const buttonSearch = screen.getByTestId(searchBarBtn);
 
     userEvent.type(inputText, 'potato');
     userEvent.click(buttonSearch);
+    await screen.findByTestId('recipe-title');
     act(() => {
       history.push('/meals');
     });
+    await screen.findByTestId('button-search');
+    userEvent.click(screen.getByTestId('button-search'));
     userEvent.type(inputText, 'potato');
     userEvent.click(screen.getByTestId('name-search-radio'));
     userEvent.click(buttonSearch);
-    await waitFor(() => expect(screen.findByTestId(imageCard)), { interval: 3000 });
+    await screen.findByTestId('0-card-img');
     userEvent.click(screen.getByTestId(firstLetter));
     userEvent.type(inputText, 'a');
     userEvent.click(buttonSearch);
     await waitFor(() => expect(screen.findByTestId(imageCard)), { interval: 3000 });
   });
   it('testa drink end to end', async () => {
-    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
+    global.alert = jest.fn();
     const loginInput = 'login-submit-btn';
     expect(screen.getByTestId(emailInput));
     expect(screen.getByTestId(password));
@@ -56,20 +62,42 @@ describe('end to end basicao', () => {
     expect(screen.getByTestId(loginInput)).toBeEnabled();
     userEvent.click(screen.getByTestId(loginInput));
 
+    await screen.findByTestId('drinkButton');
     userEvent.click(screen.getByTestId('drinkButton'));
+    await screen.findByTestId('button-search');
     userEvent.click(screen.getByTestId('button-search'));
     const buttonSearch = screen.getByTestId(searchBarBtn);
     userEvent.type(screen.getByTestId(searchInput), 'lime');
     userEvent.click(buttonSearch);
     await waitFor(() => expect(screen.findByTestId(imageCard)), { interval: 3000 });
-    userEvent.click(screen.getByTestId(firstLetter));
-    userEvent.click(screen.getByTestId('ingredient-search-radio'));
+    // userEvent.click(screen.getByTestId(firstLetter));
+    // userEvent.click(screen.getByTestId('ingredient-search-radio'));
     userEvent.click(screen.getByTestId(firstLetter));
     userEvent.type(screen.getByTestId(searchInput), 'a');
     userEvent.click(buttonSearch);
     await waitFor(() => expect(screen.findByTestId(imageCard)), { interval: 3000 });
     userEvent.click(screen.getByTestId(firstLetter));
     userEvent.type(screen.getByTestId(searchInput), 'aa');
+    userEvent.click(buttonSearch);
+    await waitFor(() => expect(global.alert).toHaveBeenCalled());
+    userEvent.click(screen.getByTestId('name-search-radio'));
+    userEvent.clear(screen.getByTestId(searchInput));
+    userEvent.type(screen.getByTestId(searchInput), 'Aquamarine');
+    userEvent.click(buttonSearch);
+    await screen.findByText(/Aquamarine/i);
+    act(() => {
+      history.push('/drinks');
+    });
+    await screen.findByTestId('drinkButton');
+    userEvent.click(screen.getByTestId('drinkButton'));
+    await screen.findByTestId('button-search');
+    userEvent.click(screen.getByTestId('button-search'));
+    await screen.findByTestId('ingredient-search-radio');
+    userEvent.click(screen.getByTestId(firstLetter));
+    userEvent.click(screen.getByTestId('ingredient-search-radio'));
+    userEvent.type(screen.getByTestId(searchInput), 'lime');
+    userEvent.click(buttonSearch);
+    await waitFor(() => expect(screen.findByTestId(imageCard)), { interval: 3000 });
   });
   it('teste mocked', async () => {
     global.alert = jest.fn();
@@ -88,15 +116,15 @@ describe('end to end basicao', () => {
     userEvent.click(screen.getByTestId('search-top-btn'));
     await waitFor(() => screen.getByTestId('name-search-radio'));
     userEvent.click(screen.getByTestId('name-search-radio'));
-    userEvent.type(screen.getByTestId('search-input'), 'xablau');
+    userEvent.type(screen.getByTestId(searchInput), 'xablau');
     userEvent.click(screen.getByTestId('exec-search-btn'));
     await waitFor(() => expect(global.alert).toHaveBeenCalled());
     userEvent.click(screen.getByTestId('drinkButton'));
     userEvent.click(screen.getByTestId('search-top-btn'));
     await waitFor(() => screen.getByTestId('name-search-radio'));
     userEvent.click(screen.getByTestId('name-search-radio'));
-    userEvent.type(screen.getByTestId('search-input'), 'xablau');
+    userEvent.type(screen.getByTestId(searchInput), 'xablau');
     userEvent.click(screen.getByTestId('exec-search-btn'));
-    await waitFor(() => expect(global.alert).toHaveBeenCalled());
+    await waitFor(() => expect(global.alert).toHaveBeenCalledTimes(2));
   });
 });
