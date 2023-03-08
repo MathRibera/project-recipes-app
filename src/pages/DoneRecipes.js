696d51b19a34320199b73072d9eef7f4ae61ffee
Giovanna Sousa <giovannasousa54@gmail.com>
2023-03-08 22:03:05 +0100

    requisito 44 concluído

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -6,6 +6,27 @@ export default function DoneRecipes() {
   return (
     <div>
       <Header title="Done Recipes" searchIcon={ false } />
+      <div>
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+        >
+          All
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-meal-btn"
+        >
+          Meals
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+        >
+          Drinks
+        </button>
+        <img data-testid={ `${index}horizontal-image-` } alt="hi" />
+      </div>
     </div>
   );
 }
